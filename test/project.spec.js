24ba1490f5f297d06aac1d6c122a96e7ee1091b8
piotrekbass <piotrek.koscielniak@gmail.com>
2015-03-16 22:07:42 +0100

    test setup on supertest

--- test/project.spec.js
+++ test/project.spec.js
@@ -1,8 +1,13 @@
 var expect = require('chai').expect;
+var app = require('../app');
+var request = require('supertest').agent(app.listen());
 
 describe('project', function () {
 
     it('test setup', function () {
-        expect(true).to.be.ok;
+        request
+            .get()
+            .expect(200)
+            .expect('Hello World');
     });
 });
