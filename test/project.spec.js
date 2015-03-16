eaa09f2373eb9f10a554a103a7b00f60048fe73e
piotrekbass <piotrek.koscielniak@gmail.com>
2015-03-16 21:10:42 +0100

    test setup

--- test/project.spec.js
+++ test/project.spec.js
@@ -3,7 +3,6 @@ var expect = require('chai').expect;
 describe('project', function () {
 
     it('test setup', function () {
-        expect({}).to.be.ok;
+        expect(true).to.be.ok;
     });
-
 });
