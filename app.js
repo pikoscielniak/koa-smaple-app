9a4af94b185f44f04625aeef1ad6775443dd9424
piotrekbass <piotrek.koscielniak@gmail.com>
2015-03-16 22:59:02 +0100

    getting projects

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var koa = require('koa');
+var route = require('koa-route');
+var monk = require('monk');
 var app = koa();
+var db = monk(process.env.DB_CONNECTION)
+var wrap = require('co-monk');
+
 module.exports = app;
-app.use(function *() {
-    this.body = 'Hello World';
-});
+
+function * getProjects() {
+
+}
+
+app.use(route.get('/project', function * () {
+    var projects = wrap(db.get('projects'));
+    this.body = yield projects.find({});
+}));
+
 
 app.listen(3000);
--- test/project.spec.js
+++ test/project.spec.js
@@ -1,13 +1,35 @@
 var expect = require('chai').expect;
 var app = require('../app');
 var request = require('supertest').agent(app.listen());
+var co = require('co');
+var monk = require('monk');
+var db = monk(process.env.DB_CONNECTION)
+var wrap = require('co-monk');
 
 describe('project', function () {
 
-    it('test setup', function () {
-        request
-            .get()
-            .expect(200)
-            .expect('Hello World');
+    describe('get /project', function () {
+
+        beforeEach(function (done) {
+
+            co(function *() {
+                var projects = wrap(db.get('projects'));
+                yield projects.remove({});
+                yield projects.insert({title: 'First project'});
+                yield projects.insert({title: 'Second project'});
+            }).then(done, done);
+        });
+
+        it('returns all existing projects', function (done) {
+            request.get('/project')
+                .expect(200)
+                .expect(function (res) {
+                    var projects = res.body;
+                    expect(projects).to.have.length(2);
+                    expect(projects[0].title).to.equal('First project');
+                    expect(projects[1].title).to.equal('Second project');
+                })
+                .end(done);
+        });
     });
 });
