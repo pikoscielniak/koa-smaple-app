c07c6a8228c9e990b7bd34babe81c5f934cf9ec9
piotrekbass <piotrek.koscielniak@gmail.com>
2015-03-24 23:41:15 +0100

    test views

--- server.js
+++ server.js
@@ -2,18 +2,27 @@ var koa = require('koa');
 var route = require('koa-route');
 var db = require('./lib/db');
 var serve = require('koa-static');
+var views = require('co-views');
+var render = views(__dirname + '/views', {ext: 'ejs'});
 
 var app = koa();
 module.exports = app;
 
-app.use(serve(__dirname + '/www'));
+app.use(serve(__dirname + '/public'));
 
 function * getProjects() {
     return yield db.projects.find({});
 }
 
-app.use(route.get('/project', function * () {
+app.use(route.get('/api/project', function * () {
     this.body = yield getProjects();
+}));
+
+app.use(route.get('/projects', function *() {
+    var vm = {
+        title: 'Test'
+    };
+    this.body = yield render('projects', vm);
 }));
 
 
--- test/project.spec.js
+++ test/project.spec.js
@@ -7,29 +7,15 @@ var db = require('../lib/db');
 
 describe('project', function () {
 
-    describe('get /project', function () {
+    describe('get /projects', function () {
 
-        beforeEach(function (done) {
 
-            co(function *() {
-                var projects = db.projects;
-                yield projects.remove({});
-                var yieldables = [
-                    projects.insert({title: 'First project'}),
-                    projects.insert({title: 'Second project'})
-                ];
-                yield yieldables;
-            }).then(done, done);
-        });
-
-        it('returns all existing projects', function (done) {
-            request.get('/project')
+        it('shows all existing projects', function (done) {
+            request.get('/projects')
                 .expect(200)
+                //.expect('Content-Type', /html/)
                 .expect(function (res) {
-                    var projects = res.body;
-                    expect(projects).to.have.length(2);
-                    expect(projects[0].title).to.equal('First project');
-                    expect(projects[1].title).to.equal('Second project');
+                    expect(res.text).to.be.eql('<h3>Test</h3>');
                 })
                 .end(done);
         });
