92e20fea9506a5a6ca9d5d39d4b745a0af51e72b
piotrekbass <piotrek.koscielniak@gmail.com>
2015-03-28 17:59:34 +0100

    refactoring in tests

--- test/project.spec.js
+++ test/project.spec.js
@@ -4,6 +4,19 @@ var request = require('supertest').agent(app.listen());
 var co = require('co');
 
 var db = require('../lib/db');
+var testProject1 = {name: 'First project', url: 'http://project1.com'};
+var testProject2 = {name: 'First project', url: 'http://project1.com'};
+
+function * addTestProjects() {
+    var projects = db.projects;
+    yield projects.remove({});
+    var yieldables = [
+        projects.insert(testProject1),
+        projects.insert(testProject2)
+    ];
+    var result = yield yieldables;
+    return result;
+}
 
 describe('project api', function () {
 
@@ -11,13 +24,7 @@ describe('project api', function () {
 
         beforeEach(function (done) {
             co(function *() {
-                var projects = db.projects;
-                yield projects.remove({});
-                var yieldables = [
-                    projects.insert({name: 'First project', url: 'http://project1.com'}),
-                    projects.insert({name: 'Second project', url: 'http://project2.com'})
-                ];
-                yield yieldables;
+                yield addTestProjects();
             }).then(done, done);
         });
 
@@ -27,10 +34,10 @@ describe('project api', function () {
                 .expect(function (res) {
                     var projects = res.body;
                     expect(projects).to.have.length(2);
-                    expect(projects[0].name).to.equal('First project');
-                    expect(projects[0].url).to.equal('http://project1.com');
-                    expect(projects[1].name).to.equal('Second project');
-                    expect(projects[1].url).to.equal('http://project2.com');
+                    expect(projects[0].name).to.equal(testProject1.name);
+                    expect(projects[0].url).to.equal(testProject1.url);
+                    expect(projects[1].name).to.equal(testProject1.name);
+                    expect(projects[1].url).to.equal(testProject2.url);
                 })
                 .end(done);
         });
@@ -45,12 +52,8 @@ describe('project api', function () {
         });
 
         it("adds new projects", function (done) {
-            var testProject = {
-                name: 'test project',
-                url: 'http://test-project.com'
-            };
             request.post('/api/project')
-                .send(testProject)
+                .send(testProject1)
                 .expect('Location', /^\/api\/project\/[0-9a-fA-F]{24}$/)
                 .end(function (err, res) {
                     var location = res.header.location;
@@ -59,17 +62,14 @@ describe('project api', function () {
                         var id = location.match(/[0-9a-fA-F]{24}$/)[0];
                         var projects = yield db.projects.find({_id: id});
                         var justCreatedProject = projects[0];
-                        expect(justCreatedProject.name).to.equal(testProject.name);
-                        expect(justCreatedProject.url).to.equal(testProject.url);
+                        expect(justCreatedProject.name).to.equal(testProject1.name);
+                        expect(justCreatedProject.url).to.equal(testProject1.url);
                     }).then(done, done);
                 });
         });
 
         it("not allow project with existing name", function (done) {
-            var existingProject = {
-                name: 'test project',
-                url: 'http://test-project.com'
-            };
+            var existingProject = testProject1;
 
             co(function *() {
                 yield db.projects.insert(existingProject);
@@ -87,10 +87,7 @@ describe('project api', function () {
 
 
         it("not allow project with existing url", function (done) {
-            var existingProject = {
-                name: 'test project',
-                url: 'http://test-project.com'
-            };
+            var existingProject = testProject1;
 
             co(function *() {
                 yield db.projects.insert(existingProject);
@@ -113,13 +110,7 @@ describe('project api', function () {
 
         beforeEach(function (done) {
             co(function *() {
-                var projects = db.projects;
-                yield projects.remove({});
-                var yieldables = [
-                    projects.insert({name: 'First project', url: 'http://project1.com'}),
-                    projects.insert({name: 'Second project', url: 'http://project2.com'})
-                ];
-                insertedProjects = yield yieldables;
+                insertedProjects = yield addTestProjects();
             }).then(done, done);
         });
 
@@ -128,8 +119,8 @@ describe('project api', function () {
                 .expect(200)
                 .expect(function (res) {
                     var project = res.body;
-                    expect(project.name).to.equal('First project');
-                    expect(project.url).to.equal('http://project1.com');
+                    expect(project.name).to.equal(testProject1.name);
+                    expect(project.url).to.equal(testProject1.url);
                 })
                 .end(done);
         });
