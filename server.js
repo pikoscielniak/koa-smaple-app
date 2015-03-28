ffa036ea45e297b8deacc4b153f4f26dd28b95d4
piotrekbass <piotrek.koscielniak@gmail.com>
2015-03-28 13:01:15 +0100

    adding new project

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var koa = require('koa');
 var route = require('koa-route');
-var db = require('./lib/db');
 var serve = require('koa-static');
+var parse = require('co-body');
+var db = require('./lib/db');
 
 var app = koa();
 module.exports = app;
@@ -15,6 +16,19 @@ function * getProjects() {
 app.use(route.get('/api/project', function * () {
     this.body = yield getProjects();
 }));
+
+app.use(route.post('/api/project', function * () {
+    var postedProject = yield parse(this);
+    var newProject = {
+        name: postedProject.name,
+        url: postedProject.url
+    };
+
+    var savedProject = yield db.projects.insert(newProject);
+    this.set('Location', '/api/project/' + savedProject._id);
+    this.status = 201;
+}));
+
 
 app.listen(3000);
 console.log("Working...");
--- test/project.spec.js
+++ test/project.spec.js
@@ -14,8 +14,8 @@ describe('project api', function () {
                 var projects = db.projects;
                 yield projects.remove({});
                 var yieldables = [
-                    projects.insert({title: 'First project'}),
-                    projects.insert({title: 'Second project'})
+                    projects.insert({name: 'First project', url: 'http://project1.com'}),
+                    projects.insert({name: 'Second project', url: 'http://project2.com'})
                 ];
                 yield yieldables;
             }).then(done, done);
@@ -27,10 +27,43 @@ describe('project api', function () {
                 .expect(function (res) {
                     var projects = res.body;
                     expect(projects).to.have.length(2);
-                    expect(projects[0].title).to.equal('First project');
-                    expect(projects[1].title).to.equal('Second project');
+                    expect(projects[0].name).to.equal('First project');
+                    expect(projects[0].url).to.equal('http://project1.com');
+                    expect(projects[1].name).to.equal('Second project');
+                    expect(projects[1].url).to.equal('http://project2.com');
                 })
                 .end(done);
+        });
+    });
+
+    describe('post /api/project', function () {
+        beforeEach(function (done) {
+            co(function *() {
+                var projects = db.projects;
+                yield projects.remove({});
+            }).then(done, done);
+        });
+
+        it("adds new projects", function (done) {
+            var testProject = {
+                name: 'test project',
+                url: 'http://test-project.com'
+            };
+            request.post('/api/project')
+                .send(testProject)
+                .expect('Location', /^\/api\/project\/[0-9a-fA-F]{24}$/)
+                .expect(201)
+                .end(function (err, res) {
+                    var location = res.header.location;
+                    co(function * () {
+                        var id = location.match(/[0-9a-fA-F]{24}$/)[0];
+                        var projects = yield db.projects.find({_id: id});
+                        var justCreatedProject = projects[0];
+                        expect(justCreatedProject.name).to.equal(testProject.name);
+                        expect(justCreatedProject.url).to.equal(testProject.url);
+                    }).then(done, done);
+                });
+            //.end(done);
         });
     });
 });
