fc3a7111fb03c7e46dd702b5fc2b6ba075d9d38d
piotrekbass <piotrek.koscielniak@gmail.com>
2015-04-06 12:16:55 +0200

    adding vote happy way

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var koa = require('koa');
 var router = require('koa-router')();
 var serve = require('koa-static');
 var project = require('./lib/project');
+var vote = require('./lib/vote');
 
 var authService = require('./lib/auth/authService');
 var authorize = require('./lib/auth/authorize');
@@ -17,6 +18,8 @@ app.use(serve(__dirname + '/public'));
 router.get('/api/project', project.getAll);
 router.get('/api/project/:id', project.getById);
 router.post('/api/project', authorize, project.post);
+
+router.post('/api/vote', authorize, vote.addVote);
 
 app.use(router.routes());
 app.use(router.allowedMethods());
--- test/voting.spec.js
+++ test/voting.spec.js
@@ -4,24 +4,65 @@ var request = require('supertest').agent(app.listen());
 var co = require('co');
 var db = require('../lib/db');
 var testHelpers = require('./testHelpers');
+var jwtTokenService = require('../lib/auth/jwtTokenService');
 
+describe.only('voting', function () {
 
-describe('voting', function () {
-
-    var testUser = {
+    var testUser1 = {
         email: 'test@test.pl',
         password: 'test'
     };
+    var testUser2 = {
+        email: 'test2@test.pl',
+        password: 'test'
+    };
+
+    var testProject = {
+        name: 'First project', url: 'http://project1.com'
+    };
+
+    var authorToken;
+    var voterToken;
+    var projectId;
+    var parsedVoterToken;
+
+    function addProject(request, project, token) {
+        return new Promise(function (resolve, reject) {
+            request.post('/api/project')
+                .set('Authorization', 'Bearer ' + token)
+                .send(project)
+                .end(function (err, res) {
+                    if (err) return reject(err);
+                    var projectId = res.headers.location.split('/')[3];
+                    resolve(projectId);
+                });
+        });
+    }
 
     beforeEach(function (done) {
         co(function *() {
             yield testHelpers.clearEnvironment();
-        }).then(function () {
-
-        }, done);
+            authorToken = yield testHelpers.registerUser(request, testUser1);
+            voterToken = yield testHelpers.registerUser(request, testUser2);
+            parsedVoterToken = jwtTokenService.decodeJwtToken(voterToken);
+            projectId = yield addProject(request, testProject, authorToken);
+        }).then(done, done);
     });
 
-    it("add votes", function (done) {
-        done();
+    it("add vote", function (done) {
+        request.post('/api/vote')
+            .send({projectId: projectId})
+            .expect('Location', /^\/api\/vote\/[0-9a-fA-F]{24}$/)
+            .set('Authorization', 'Bearer ' + voterToken)
+            .end(function (err, res) {
+                co(function * () {
+                    expect(res.status).to.equal(201);
+                    var alreadyAddedVote = yield db.votes.findOne({
+                        projectId: projectId,
+                        voterId: parsedVoterToken.sub
+                    });
+                    expect(alreadyAddedVote).to.be.ok;
+                }).then(done, done);
+            });
     });
 });
